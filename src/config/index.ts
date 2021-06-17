6821f4c08632a543bac150679a70234aff1fbe3c
Damir <damir@yusipov.ru>
2021-06-17 20:24:09 +0300

    port fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,14 +3,11 @@ import $ from "../lib/config"
 export const config = { 
   // server
   nodeEnv: $("NODE_ENV", String, "dev"),
-  port: $("SERVER_PORT", Number, 3000),
+  port: $("PORT", Number, 3000),
   baseUrl: $("BASE_URL"),
   logLevel: $("LOG_LEVEL", String, "info"),
 
   // agenda 
-  reminderInviteDelay: $("REMINDER_INVITE_DELAY", Number, 3 * 24 * 60 * 60 * 1000), // 3 days
-  checkNewMatchesInterval: $("CHECK_NEW_MATCHES_INTERVAL", String, "3 minutes"),
-
   // mongodb
   mongoUrl: $("MONGO_URL"),
   mongoTestUrl: $("MONGO_TEST_URL"),
